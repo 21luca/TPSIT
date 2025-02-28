f56faed4d4c5e5e8b1daab432530c9b3ce02365d
21luca <162013769+21luca@users.noreply.github.com>
2025-02-28 16:19:28 +0000

    Esercizi

--- ESERCIZI/Forme.java
+++ ESERCIZI/Forme.java
@@ -30,7 +30,7 @@ public class Forme {
     public static void main(String[] args) {
     Forma[] forme = { new Cerchio(3), new Rettangolo(1, 2) };
         for (Forma forma : forme) {
-            System.out.println("Area: " + forma.calcolaArea());
+            System.out.println("area= " + forma.calcolaArea());
         }
     }
 }
